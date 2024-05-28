97781f05ff2126792aec5260c5f0697003de39b2
StatesTania <stts.tania@gmail.com>
2024-05-28 14:03:23 +0200

    errorMsg

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,26 @@ console.log("Hello! script chargé");
 
 const myForm = document.querySelector("form");
 console.log("variable myForm:", myForm);
-const errorMessagesDiv = document.getElementById("error-message");
-console.log("ceci est un message d'erreur");
 
-myForm.addEventListener("submit", (event) => {
+myForm.addEventListener("submit", checkInputs);
+
+function checkInputs(event) {
   event.preventDefault();
-
-  console.log("mon event contient:", event);
+  event.sopPropagation();
 
   let inputFirstName = document.querySelector("#first-name").value.trim();
   console.log("inputFirstName contien:", inputFirstName);
-
   let inputLastName = document.querySelector("#last-name").value.trim();
   console.log("inputLastName contien:", inputLastName);
-
   let inputmessage = document.querySelector("#message").value.trim();
   console.log("inputmessage contien:", inputmessage);
 
-  if (inputFirstName === "" || inputLastName === "" || inputmessage === "") {
+  const errorMsg = document.getElementById("error-message");
+
+  if ((inputFirstName || inputLastName || inputmessage) === "") {
+    //add comment
     console.log("Un champ est vide");
+    console.log(errorMsg);
+    errorMsg.style.display = "none";
   }
-});
+}
